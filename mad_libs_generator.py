3520513f802af94313bee4371d9251d6e72fcf1c
Kshitij Thakre <ktkthakre@gmail.com>
2020-08-04 19:02:36 +0530

    Add files via upload

---
+++ mad_libs_generator.py
@@ -0,0 +1,13 @@
+"""
+Simple Mad Libs Generating Game
+"""
+print("Welcome to the Mad Libs Generator")
+name1 = input("Enter a Male Name: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+name2 = input("Enter another name: ")
+country = input("Enter name of country: ")
+
+print("Once there was a guy whose name was "+name1+". He was mighty and "+adjective)
+print("He was "+verb+" with his wife and suddenly "+name2+" saw them!")
+print("So "+name1+" killed "+name2+" and fled the country of "+country)
